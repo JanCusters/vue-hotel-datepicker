3f97dd0d3f0d5172b6dadcf1de96544461c81711
Jan Custers <jncusters@gmail.com>
2018-05-29 10:37:54 +0200

    throw event on date selection clear

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -320,6 +320,7 @@ export default {
       this.show = true;
       this.parseDisabledDates();
       this.reRender()
+      this.$emit('selectionCleared')
     },
 
     hideDatepicker() { this.isOpen = false; },
